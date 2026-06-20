f3f4e509f0c50d6590d5c1a730b245bd8bde02e3
MathiasVilca <randommathias31@gmail.com>
2026-06-19 19:02:20 -0500

    style(jitter):increased multiplier and made radius non-linear

--- src/components/AuraCanvas.jsx
+++ src/components/AuraCanvas.jsx
@@ -18,7 +18,7 @@ const AuraCanvas = ({ parameters, size, reduction_parameter=1,reduce_particles=f
     const particleBaseCount = reduce_particles? reducedBaseParticleCount : 55
     const particleBaseMultiplier = reduce_particle_multiplier? reducedBaseParticleMult : 65
     const particleCount = particleBaseCount + Math.round(parameters.energy * particleBaseMultiplier * reduction_parameter); // Ajustar cantidad de partículas según energía y reducción
-    const jitterMultiplier = 0.25
+    const jitterMultiplier = 0.5
     const particles = Array.from({ length: particleCount }).map(() => {
       
       // 1. Calculamos la distancia inicial según el patrón activo
@@ -113,7 +113,7 @@ const AuraCanvas = ({ parameters, size, reduction_parameter=1,reduce_particles=f
           }
         }
         //jitter, necesita ajustes
-        const jitterRadius = p.radius * parameters.stress * jitterMultiplier //radio de que posicion puede cambiar (EJEMPLO)
+        const jitterRadius = p.radius * Math.pow(parameters.stress,1.75) * jitterMultiplier //radio de que posicion puede cambiar (EJEMPLO)
         //como p.radius ya afectado por reduction_parameter...
         //considerando que stress esta entre 0 y 1
         //angulo al azar 
